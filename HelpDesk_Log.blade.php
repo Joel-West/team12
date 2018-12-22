901b4f2c4270e57b74294046c7d44c33d149d3e1
turnip99 <turnipz99@gmail.com>
2018-12-22 14:51:18 +0000

    Add files via upload

---
+++ HelpDesk_Log.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta content="text/html" charset="UTF-8" />
+		<title>HelpDesk_LogIn</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="ExtraCode.js"></script>
+		<script type="text/javascript">
+			function Validate(){
+				var Username=document.getElementById("Username").value;
+				var Password=document.getElementById("Password").value;
+				if (Username == "Alice" && Password == "Password"){
+					location.replace("HelpDesk_Home.blade.php?name="+Username);
+				}
+			}
+		</script>
+		<link href="Styles.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h1 class="center">Login</h1>
+		<div class="center">
+			<input type="text" name="Username" id="Username" placeholder="Username" "value=""><br>
+			<input type="password" name="Password" id="Password" placeholder="Password" ><br>
+			<input type="submit" name="submit" id="submit" value="Submit" style="font-size:18px;" onClick="Validate();" /><br>
+			Save Password: <input type="checkbox" id="checkSave" />
+		</div>
+	</body>
+</html>
