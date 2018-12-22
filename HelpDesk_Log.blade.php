458141dfbe47afbca0061669ad75a94db08c72d3
turnip99 <turnipz99@gmail.com>
2018-12-22 16:24:22 +0000

    Add files via upload

--- HelpDesk_Log.blade.php
+++ HelpDesk_Log.blade.php
@@ -26,15 +26,15 @@
 			*/			
 			
 			function Validate(){
-				alert("");
 				var Username=document.getElementById("Username").value;
 				var Password=document.getElementById("Password").value;
-				if (Username == "Alice" && Password == "Password"){
-					return true;
+				if (Username == "Alice" && Password == "Password")
+				{
+					document.mainform.submit();
 				}
 				else
 				{
-					return false;
+					alert("Uhhhhhh mate what you doing?");
 				}
 			}
 		</script>
@@ -42,13 +42,13 @@
 	</head>
 	<body>
 	<!--action="header('Location: http://35.204.60.31/Home');"-->
-	<form <onsubmit="return Validate()" method="post" action="http://35.204.60.31/Home" id="form">
+	<form <method="post" action="http://35.204.60.31/Home" id="form" name="mainform">
 		@csrf
 		<h1 class="center">Login</h1>
 		<div class="center">
 			<input type="text" name="Username" id="Username" placeholder="Username" value=""><br>
 			<input type="password" name="Password" id="Password" placeholder="Password" ><br>
-			<input type="submit" name="submit" id="submit" value="Submit" style="font-size:18px;" /><br>
+			<input type="submit" name="submit" id="submit" value="Submit" style="font-size:18px;" onclick="Validate()"/><br>
 			Save Password: <input type="checkbox" id="checkSave" />
 		</div>
 	</form>
