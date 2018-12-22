f510f42f6f6c27faff5538a46fda4edd2f318388
turnip99 <turnipz99@gmail.com>
2018-12-22 16:40:43 +0000

    Add files via upload

--- HelpDesk_Log.blade.php
+++ HelpDesk_Log.blade.php
@@ -42,7 +42,7 @@
 	</head>
 	<body>
 	<!--action="header('Location: http://35.204.60.31/Home');"-->
-	<form id="mainform" name="mainform" action="http://35.204.60.31/Home">
+	<form id="mainform" name="mainform" method="post" action="http://35.204.60.31/Home.blade.php">
 		@csrf
 		<h1 class="center">Login</h1>
 		<div class="center">
