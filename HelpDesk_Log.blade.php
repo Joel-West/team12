5aeb757c8de7f0b42c2c69ac0f065a12507d2fe8
turnip99 <turnipz99@gmail.com>
2018-12-24 19:19:12 +0000

    Add files via upload

--- HelpDesk_Log.blade.php
+++ HelpDesk_Log.blade.php
@@ -18,6 +18,21 @@
 					alert("Uhhhhhh mate what you doing?");
 				}
 			}
+			function RunQuery()
+			{
+				sql = "SELECT * FROM tblPersonnel";
+				$.get("app/query.php", {'sql':sql},function(json) //Calls query.php, which handles the SQL query and sorting of result data.
+				{
+					if(json && json[0]) //If result of php was a json array		
+					{
+						alert(json[0])
+					}
+					else
+					{
+						alert("Sorry, no results found..."); //If no results, display error.
+					}
+				},'json'); 
+			}
 		</script>
 		<link rel="stylesheet" href="{{ asset('css/Styles.css') }}" type="text/css">
 	</head>
@@ -30,6 +45,7 @@
 			<input type="password" name="Password" id="Password" placeholder="Password" ><br>
 			<input type="button" name="btnsubmit" id="btnsubmit" value="Submit" style="font-size:18px;" onclick="Validate();"/><br>
 			Save Password: <input type="checkbox" id="checkSave" />
+			<input type="button" id="btntest" value="Test" onclick="RunQuery()"/>
 			<!-- <input type="submit" value="You shouldn't be able to see me..." style="visibility:hidden"/> INVISIBLE SUBMIT BUTTON SO THAT SUBMIT FUNCTION WORKS, DO NOT TOUCH ME-->
 		</div>
 	</form>
