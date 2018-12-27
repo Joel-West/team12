881c26ee4ca8f92a540edae94174abe6fbf2537b
turnip99 <turnipz99@gmail.com>
2018-12-27 15:09:38 +0000

    Add files via upload

---
+++ query.php
@@ -0,0 +1,24 @@
+<?php
+	require_once 'MDB2.php';
+	include "coa123-mysql-connect.php"; //Includes file containing username and password for database.
+	$host='localhost';
+	$dbName='coa123wdb';
+	$dsn = "mysql://$username:$password@$host/$dbName";
+	$db =& MDB2::connect($dsn); //Accesses database.
+	if(PEAR::isError($db)){ 
+		die($db->getMessage()); //Returns error if not able to access database.
+	}
+	$db->setFetchMode(MDB2_FETCHMODE_ASSOC);
+	$date = $_REQUEST['date'];
+	$grade = $_REQUEST['grade'];
+	$size = $_REQUEST['size']; //Retrieves data from form.
+	$fDate = date("Y-m-d", strtotime($date)); //Parses the date into a format that will match with the database.
+	$sql = "SELECT name, weekend_price, weekday_price, licensed, venue_id FROM venue WHERE capacity >= $size AND venue_id NOT IN (SELECT DISTINCT venue_id FROM venue_booking WHERE venue_booking.date_booked = '$fDate') AND venue_id IN (SELECT venue_id FROM catering WHERE grade = $grade)";
+	//Forms SQL query that gets most details of all venues that are not booked for the selected date, have a greater capacity than the party size and provide catering of the specified grade.
+	$res =& $db->query($sql);
+	if(PEAR::isError($res)){
+		die($res->getMessage());
+	}
+	$value = json_encode($res->fetchAll());
+	echo $value; //Returns value of json array back to the form for display.
+?>
