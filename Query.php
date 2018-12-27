07c457e5fb7cea51d062c6207be34f07aefb445f
turnip99 <turnipz99@gmail.com>
2018-12-27 14:15:24 +0000

    Update Query.php

--- Query.php
+++ Query.php
@@ -1,29 +1,20 @@
 <?php
-	//$sql = $_REQUEST['sql'];
+	$sql = $_REQUEST['sql'];
 	$host='35.204.50.1';
 	$username = "root";
 	$password = "";
-	echo json_encode("Ayyyy123");
-
-	$conn = new mysqli($host,$username,$password);
-
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	echo json_encode("Yes lads");
-	/*
+	echo json_encode("Ayyyy1");
+	
 	try 
 	{
 		$con = new PDO("mysql:host=$host;dbname=team12database;charset=utf8mb4",$username,$password);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
 		
 		$stmt = $con->prepare($sql);
 		$stmt->execute();
 		$res = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 		
 		$con = null;
-		
 		echo json_encode("Ayyyy2");
 		
 
@@ -33,5 +24,6 @@
 		echo json_encode("Connection failed".$e->getMessage());
 	}
 	echo json_encode($r);
-	*/
+	
+	
 ?>
