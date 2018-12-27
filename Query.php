2025c0605be989f3dd2f56e60cccb200f53b90b2
turnip99 <turnipz99@gmail.com>
2018-12-27 15:20:29 +0000

    Add files via upload

--- Query.php
+++ Query.php
@@ -7,18 +7,16 @@
 	{
 		$con = new PDO("mysql:host=$host;dbname=team12database;charset=utf8mb4",$username,$password);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$stmt = $con->query($sql);
-		echo json_encode($stmt);
-		foreach ($stmt as $row)
-		{
-			//echo json_encode($row);
-		}
+		$statement = $pdo->prepare($sql);
+		$statement->execute();
+		$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+		$json = json_encode($results);
 		$con = null;
-		
-
 	}
 	catch(PDOException $e)
 	{
-		echo json_encode("Connection failed".$e->getMessage());
+		$json = ("Connection failed".$e->getMessage());
 	}
+	
+	echo $json;
 ?>
