cd4a4fbe7f4b4b89cad81ac120502c336b941e1a
turnip99 <turnipz99@gmail.com>
2018-12-27 15:18:44 +0000

    Add files via upload

--- Query.php
+++ Query.php
@@ -8,7 +8,7 @@
 		$con = new PDO("mysql:host=$host;dbname=team12database;charset=utf8mb4",$username,$password);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$stmt = $con->query($sql);
-		echo json_encode $stmt;
+		echo json_encode($stmt);
 		foreach ($stmt as $row)
 		{
 			//echo json_encode($row);
