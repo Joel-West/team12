1f26215a58df99bf710d1d15238f20ba1524246f
turnip99 <turnipz99@gmail.com>
2019-01-06 18:10:34 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -11,6 +11,7 @@
 	</head>
 	<body onload="WriteTime()">
 	<form id="mainform" name="mainform" method="post" action="">
+		<input type='hidden' name="Username" value="<?php echo $_POST['Username']; ?>" />
 		@csrf
 		<div class="titleDiv">
 			<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" />
@@ -112,9 +113,8 @@
 		</table>
 		<div align="center">
 			<p>
-				Search:<input type="text"></input>
-				
-				<input type="button" value="Submit <?php echo $_POST['Username']; ?>"></input>
+				Search:<input type="text"></input>		
+				<input type="button" value="Submit"></input>
 			</p>	
 		</div>
 	</form>
