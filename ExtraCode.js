3e8ac10170d92d861efceb37fde96896a59076c6
turnip99 <turnipz99@gmail.com>
2019-01-06 19:05:32 +0000

    Add files via upload

--- ExtraCode.js
+++ ExtraCode.js
@@ -18,7 +18,7 @@ function Fun()
 	else
 	{
 		fun = false;
-		document.body.style.backgroundColor = "#FFFFFF";
+		document.body.style.backgroundColor = "#D6D6D6";
 	}
 }
 
@@ -30,6 +30,55 @@ function GetRandomCol() //Function chooses 3 random values between 60 and 255, w
 	x[2] = Math.floor(Math.random()*255);
 	return x;
 }
+
+function GetRows()
+{
+	var rows = $('#tbl tr').length;
+	return rows;
+}
+
+function Delete()
+{
+	if (selected == 0)
+	{
+		return;
+	}
+	if (confirm("Delete selected rows?"))
+	{
+		rows = GetRows();
+		for (i = rows-1; i > 0; i--)
+		{
+			if (document.getElementById("tbl").rows[i].style.backgroundColor != 'rgb(159, 255, 48)')
+			{
+				console.log("deleting t" + i);
+				document.getElementById("tbl").deleteRow(i);
+			}
+		}
+		selected = 0;
+	}
+}
+
+$(document).ready(function()
+{
+	$("#tbl").on('click','tr',function(event)
+	{
+		if ($(this).attr('id') != 't0')
+		{
+			if ($(this).css('background-color') == 'rgb(159, 255, 48)')
+			{	
+				$(this).css('background-color', '#00FFFF');
+				selected += 1;
+			}
+			else if ($(this).css('background-color') == 'rgb(0, 255, 255)')
+			{
+				$(this).css('background-color', '#9FFF30');
+				selected -= 1;
+			}
+			console.log(selected);
+		}
+	});
+});	
+
 function WriteTime()
 {
 	var dt = new Date();
@@ -39,6 +88,5 @@ function WriteTime()
 }
 function GoToNewPage(page)
 {
-	document.getElementById("mainform").action = "http://35.204.60.31/" + page;
-	document.getElementById("mainform").submit();
+	location.replace(page);
 }
