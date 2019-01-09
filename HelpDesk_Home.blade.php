ec436e577c9b4d6518b848c369e9e871b828437c
turnip99 <turnipz99@gmail.com>
2019-01-09 18:56:59 +0000

    Add files via upload

--- HelpDesk_Home.blade.php
+++ HelpDesk_Home.blade.php
@@ -9,7 +9,7 @@
 			function Load()
 			{
 				//Fun();
-				alert("<?php echo (array($_POST['User']))[0]; ?>");
+				alert("<?php echo (explode("," ,$_POST['User']))[0]; ?>");
 				WriteTime();
 			}			
 		</script>
@@ -38,7 +38,7 @@
 		<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
 			<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('');" /> <!-- Back button. -->
 			<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
-			<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, "<?php echo (array($_POST['User']))[0]; ?>"</h2> <!-- Heading containing a welcome message to the user. -->
+			<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, <?php echo (array($_POST['User']))[0]; ?></h2> <!-- Heading containing a welcome message to the user. -->
 		</div>	
 		<div class="grid-class"> <!-- Div containing main buttons that link to other pages. -->
 			<input class="mainButton" type="button" disabled value="New Call" onClick="GoToNewPage('NewCaller')" />
