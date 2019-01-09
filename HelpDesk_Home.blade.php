d6175e58adbab1b9344149ab78d3a702093d97a3
turnip99 <turnipz99@gmail.com>
2019-01-09 18:24:38 +0000

    Add files via upload

--- HelpDesk_Home.blade.php
+++ HelpDesk_Home.blade.php
@@ -33,11 +33,11 @@
 	<body onload="Load()">
 	<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
 		@csrf <!--Token to validates requests to server. -->
-		<input type='hidden' name="User" value="<?php echo $_POST['User']; ?>" /><!-- Hidden tag used to store posted user data so that it can later be posted to other pages, then back to the home page. -->
+		<input type='hidden' name="User" value="<?php echo $_POST['User']; ?>" /><!-- Hidden tag used to store posted username so that it can later be posted to other pages, then back to the home page. -->
 		<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
 			<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('');" /> <!-- Back button. -->
 			<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
-			<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, <?php echo $_POST['user']; ?></h2>	 <!-- Heading containing a welcome message to the user. -->
+			<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, <?php echo $_POST['User']; ?></h2>	 <!-- Heading containing a welcome message to the user. -->
 		</div>	
 		<div class="grid-class"> <!-- Div containing main buttons that link to other pages. -->
 			<input class="mainButton" type="button" disabled value="New Call" onClick="GoToNewPage('NewCaller')" />
