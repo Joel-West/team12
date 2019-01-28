28a470f42324a25c55033bb3f65f4af3351278b8
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-01-28 18:00:42 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="Styles.css">
+	<link rel="stylesheet" href="{{ asset('css/Styles.css') }}" type="text/css"> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
@@ -13,7 +13,7 @@
 	   jQuery('#CallerName'.on('input propertychange paste', function() {
 		var Username=document.getElementById("CallerName").value;
 		if (Username.includes("'")){
-		  }
+		}
 		else{
 		  sql = "SELECT userID FROM tblPersonnel WHERE name = '" + Username +"'"; 
 		  $.get("Query.php", {'sql':sql},function(json){
@@ -21,7 +21,7 @@
 			  if (json[1]){
 				alert("There are multiple " + Username + ". Please type their user ID as well");
 			  }
-			  else if{
+			  else{
 				document.getElementById("CallerID").value = json[0];
 			  }
 			}
