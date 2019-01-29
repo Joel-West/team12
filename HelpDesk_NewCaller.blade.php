841845cc04b375972717fda06965da644a930e96
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-01-29 13:18:35 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -66,10 +66,10 @@
 		<div class="col-2"></div>
 		<div class="col-8 d-flex">
 		  <form class="form-inline">
-		    <label for="CallerName" class="mr-sm-2 mr-auto">Caller Name:</label>
-		    <input type="text" name="CallerName" id="CallerName" onchange="autofillId()" class="form-control mb-2 mr-sm-2 mr-auto">
-			<label for="CallerID" class="mr-sm-2">Caller ID:</label>
-		    <input type="text" name="CallerID" id="CallerID" onchange="autofillName()" class="form-control mb-2 mr-sm-2">
+		    <label for="CallerName" class="mr-sm-2">Caller Name:</label>
+		    <input type="text" name="CallerName" id="CallerName" onchange="autofillId()" class="form-control mb-2 mr-sm-2">
+			<label for="CallerID" class="ml-sm-2">Caller ID:</label>
+		    <input type="text" name="CallerID" id="CallerID" onchange="autofillName()" class="form-control mb-2 ml-sm-2">
 		  </form>
 		</div>
 		<div class="col-2"></div>
