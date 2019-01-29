1c6ee3493837615ea252b07ff3122b06aa7366e4
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-01-29 12:30:21 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -69,7 +69,7 @@
 		<div class="col-6">
 		  Caller ID: <input type="text" name="CallerID" id="CallerID" onchange="autofillName()">
 		</div>
-		<div class="col-4">
+		<div class="col-4" align="center">
 		  Notes on call<br>
 		  <textarea rows="4" cols="50" style="resize:none">
 		  </textarea>
