e7b923c94d49915aa2261013da50490800506324
turnip99 <turnipz99@gmail.com>
2019-01-30 15:40:19 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -96,7 +96,7 @@
 		</div>
 		<br/>
 		<div class="row" align="center">
-		<div id="tableDiv" class="col-8" style="overflow-y: scroll; height:800px; width:700px;"> <!-- Div containing data table. -->
+		<div id="tableDiv" class="col-8" style="overflow-y: scroll; height:90%;"> <!-- Div containing data table. -->
 			Loading data...
 		</div>
 		<br/>
