f48ffb00de48acf860dca5758e7239517b89a6ad
turnip99 <turnipz99@gmail.com>
2019-01-30 14:53:44 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -91,12 +91,13 @@
 		@csrf <!--Token to validates requests to server. -->
 		<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
 			<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" /> <!-- Back button. -->
-			<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
+			<label id="dtLabel" style="font-weight: bold; font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
 			<h2 id="headerId" style="style=display:inline-block; font-size:30px;">Personnel</h2> <!-- Heading containing name of page. -->
 		</div>
 		<div id="tableDiv"> <!-- Div containing data table. -->
 			Loading data...
 		</div>
+		<br/>
 		<div align="center">
 			<p>
 				Search:<input type="text"></input> <!-- Box for searching the table for specific strings. -->
