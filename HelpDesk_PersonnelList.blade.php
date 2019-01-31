58eb90f22ab4dc727c0a33348a1b69eb49c4b4ee
turnip99 <turnipz99@gmail.com>
2019-01-31 12:18:41 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -115,7 +115,7 @@
 					if (rowNum != -1) //If row exists.
 					{
 						row = document.getElementById("tbl").rows[rowNum]; //Get row of local table that is being saved to database.
-						sql+="UPDATE tblPersonnel SET name = '$(row.cells[1].innerHTML)', jobTitle = '$(row.cells[2].innerHTML)', department = '$(row.cells[3].innerHTML)', = $(row.cells[4].innerHTML) WHERE userID == updList[i];";
+						sql+="UPDATE tblPersonnel SET name = '$(row.cells[1].innerHTML)', jobTitle = '$(row.cells[2].innerHTML)',";// department = '$(row.cells[3].innerHTML)', = $(row.cells[4].innerHTML) WHERE userID == updList[i];";
 					}
 				}
 				for (i = 0; i < GetRows(); i++)
