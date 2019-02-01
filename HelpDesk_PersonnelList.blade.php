96642e340761159d814364b06d455865d9e94483
turnip99 <turnipz99@gmail.com>
2019-02-01 14:31:43 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -51,7 +51,7 @@
 			
 			$(document).on('DOMSubtreeModified','td',function()
 			{
-				alert($(this).attr('id'));
+				console.log($(this).attr('id'));
 			});
 			
 			function AddRow() //Adds a new row to the table, from data in the text boxes.
