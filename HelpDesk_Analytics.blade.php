25f7cf0423e8fc8fe301fa928f3ec07a93a2a24b
turnip99 <turnipz99@gmail.com>
2019-02-01 13:57:53 +0000

    Add files via upload

--- HelpDesk_Analytics.blade.php
+++ HelpDesk_Analytics.blade.php
@@ -21,10 +21,11 @@
 				{
 					if(json) //If result of php file was a json array.	
 					{			
-						rec = json[0];
 						if (json[0].serialNumber == "") //If the most popular serial number is "" (all software/network problems have this value).
 						{
 							rec = json[1]; //Use the 2nd most common serial number.
+							alert(json[1].serialNumber);
+							alert(rec.serialNumber);
 						}
 						else
 						{
