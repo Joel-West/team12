d3a894001afd54c3a078d99d2d0ef3d6402a2d86
turnip99 <turnipz99@gmail.com>
2019-02-03 17:56:55 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -242,7 +242,7 @@
 	<body onload="Load()" style="height:100%;">
 		<div class="container"> <!-- Container holds elements together using Bootstrap. -->
 			<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
-				<input type='hidden' name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
+				<input type='hidden' id="user" name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
 				@csrf <!--Token to validates requests to server. -->
 				<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
 					<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" /> <!-- Back button. -->
