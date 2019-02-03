b50c5e2e7644447f56caf283a934e7907a9598fa
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-03 23:34:08 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -178,7 +178,7 @@
 		<div class="col-3"></div>
 		<div class="collapse col-6" id="newProblemCollapse">
 		  Select Problem:
-		  <div id="chooseNewProblemComboCombo"></div>
+		  <div id="chooseNewProblemCombo"></div>
 		</div>
 		<div class="col-3"></div>
 		
