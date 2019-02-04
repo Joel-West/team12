dcc2b0349ce3305025fd04b97bb011a276b03741
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-04 16:55:28 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -92,13 +92,10 @@
 		},'json');
 	  }
 	  
-	  $(document).ready(function(){
-		console.log("Hello");
-        $(".dropdown-menu").on('click', 'li a', function(){
-          $(".btn:first-child").text($(this).text());
-		  $(".btn:first-child").val($(this).text());
-		  problem();
-        });
+      $(".dropdown-menu").on('click', 'li a', function(){
+      $(".btn:first-child").text($(this).text());
+      $(".btn:first-child").val($(this).text());
+	  problem();
       });
 	  
 	  function problem(){
