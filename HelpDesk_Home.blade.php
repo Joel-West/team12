b89aa9029261d71f70aec3eb4ee8667101828da6
turnip99 <turnipz99@gmail.com>
2019-02-04 13:01:52 +0000

    Add files via upload

--- HelpDesk_Home.blade.php
+++ HelpDesk_Home.blade.php
@@ -4,6 +4,8 @@
 		<meta content="text/html" charset="UTF-8" />
 		<title>HelpDesk_Home</title>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <!-- Get JQuery library from google. -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>	<!-- Importing Bootstrap files. -->
 		<script type="text/javascript" src="{{ URL::asset('js/ExtraCode.js') }}"></script> <!-- Import JS file containing functions that are used in multiple other files -->
 		<script type="text/javascript">
 			var userData; //Variable containing data about user.
@@ -30,6 +32,7 @@
 				}
 			}
 		</script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> <!-- Bootstrap CSS stylesheet. -->
 		<link rel="stylesheet" href="{{ asset('css/Styles.css') }}" type="text/css"> <!-- Import external CSS stylesheet that contains presentation info that applies to all the pages. -->
 		<!-- CSS that defines appearance and placement of main buttons.-->
 		<style type="text/css">
@@ -41,21 +44,7 @@
 				width: 80%;
 				margin: auto;
 			}
-			.subButton
-			{
-				font-size:32px;
-				font-weight:bold;
-				padding: 12px 25px;
-				width: 40%;
-				margin: auto;
-			}
 			.grid-class
-			{
-				position: relative;
-				display: grid;
-				grid-template-columns: auto;
-			}
-			.sub-grid-class
 			{
 				position: relative;
 				display: grid;
@@ -64,28 +53,27 @@
 		</style>
 	</head>
 	<body onload="Load()">
-	<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
-		@csrf <!--Token to validates requests to server. -->
-		<input type='text' hidden id="user" name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
-		<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
-			<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('');" /> <!-- Back button. -->
-			<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
-			<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, <?php echo (explode(",", $_POST['User']))[0]; ?>!</h2> <!-- Heading containing a welcome message to the user. -->
-		</div>	
-		<div class="grid-class"> <!-- Div containing main buttons that link to other pages. -->
-			<input class="mainButton" type="button" id="btnNewCall" value="New Call" onClick="GoToNewPage('NewCaller')" />
-			<input class="mainButton" type="button" value="View Call History" onClick="GoToNewPage('CallHistory');" />
-			<input class="mainButton" type="button" value="View Problems List" onClick="GoToNewPage('ProblemList');" />
-			<input class="mainButton" type="button" value="View Personnel" onClick="GoToNewPage('PersonnelList');" />
-			<input class="mainButton" type="button" value="View Equipment" onClick="GoToNewPage('EquipmentList');" />
-			<input class="mainButton" type="button" id="btnAnalytics" value="Analytics" onClick="GoToNewPage('Analytics')" />
+		<div class="container-fluid"> <!-- Container holds elements together using Bootstrap. -->
+			<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
+				@csrf <!--Token to validates requests to server. -->
+				<input type='text' hidden id="user" name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
+				<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
+					<input type="button" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('');" /> <!-- Back button. -->
+					<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
+					<h2 id="header" style="style=display:inline-block; font-size:36px;">Welcome, <?php echo (explode(",", $_POST['User']))[0]; ?>!</h2> <!-- Heading containing a welcome message to the user. -->
+				</div>	
+				<div class="grid-class"> <!-- Div containing main buttons that link to other pages. -->
+					<input class="mainButton" type="button" id="btnNewCall" value="New Call" onClick="GoToNewPage('NewCaller')" />
+					<input class="mainButton" type="button" value="View Call History" onClick="GoToNewPage('CallHistory');" />
+					<input class="mainButton" type="button" value="View Problems List" onClick="GoToNewPage('ProblemList');" />
+					<input class="mainButton" type="button" value="View Personnel" onClick="GoToNewPage('PersonnelList');" />
+					<input class="mainButton" type="button" value="View Equipment" onClick="GoToNewPage('EquipmentList');" />
+					<input class="mainButton" type="button" id="btnAnalytics" value="Analytics" onClick="GoToNewPage('Analytics')" />			
+					<input class="mainButton" type="button" value="Users" onClick="GoToNewPage('UserList');" />
+					<input class="mainButton" type="button" value="Specialisations" onClick="GoToNewPage('SpecialisationList');" />
+				<br>
+				<!--Fun mode (don't click if you have epilepsy...): <input id="checkFun" type="checkbox" onclick="Fun()"/>-->
+			</form>
 		</div>
-		<div class="sub-grid-class"> <!-- Div containing minor buttons that link to other pages. -->
-			<input class="subButton" type="button" value="Users" onClick="GoToNewPage('UserList');" />
-			<input class="subButton" type="button" value="Specialisations" onClick="GoToNewPage('SpecialisationList');" />
-		</div>
-		<br>
-		<!--Fun mode (don't click if you have epilepsy...): <input id="checkFun" type="checkbox" onclick="Fun()"/>-->
-	</form>
 	</body>
 </html>
