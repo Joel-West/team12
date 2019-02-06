e31648f547d7426151bf2f31e91042e1e4f0e995
turnip99 <turnipz99@gmail.com>
2019-02-06 19:16:48 +0000

    Add files via upload

--- HelpDesk_UserList.blade.php
+++ HelpDesk_UserList.blade.php
@@ -75,10 +75,12 @@
 						if (str[i].toUpperCase == "YES") //As database contains booleans as strings for the 'admin' field, unlike the local table, this is an approximate algorithm for searching 'yes' and 'no'.
 						{
 							sql += "upper(userID) LIKE '%"+str[i]+"%' OR upper(username) LIKE '%"+str[i]+"%' OR upper(password) LIKE '%"+str[i]+"%' OR upper(admin) LIKE '1'"; //Query that returns all database records with a cell containing search string.
+							console.log("yeeee")
 						}
 						else if (str[i].toUpperCase == "NO")
 						{
 							sql += "upper(userID) LIKE '%"+str[i]+"%' OR upper(username) LIKE '%"+str[i]+"%' OR upper(password) LIKE '%"+str[i]+"%' OR upper(admin) LIKE '0'"; //Query that returns all database records with a cell containing search string.
+							console.log("noooo")
 						}
 						else
 						{
