cab19c61c0081c093452251088e844152e521e9a
turnip99 <turnipz99@gmail.com>
2019-02-06 21:27:43 +0000

    Add files via upload

--- HelpDesk_PersonnelList.blade.php
+++ HelpDesk_PersonnelList.blade.php
@@ -257,7 +257,7 @@
 				{
 					return;
 				}
-				if (confirm("If any of these rows are found in the table of users, they will also be deleted. Delete selected rows?")) //Get user confirmation.
+				if (confirm("If any of these personnel are found in the table of users or specialisations, they will also be deleted. Delete selected rows?")) //Get user confirmation.
 				{
 					rows = GetRows();
 					for (i = rows-1; i > 0; i--) //Iterate through the rows of the table.
@@ -365,7 +365,7 @@
 		.table-wrapper-scroll-y
 		{
 			display: block;
-			max-height: 800px;
+			max-height: 850px;
 			overflow-y: auto;
 			-ms-overflow-style: -ms-autohiding-scrollbar;
 		}
@@ -379,7 +379,7 @@
 				<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
 					<input type="button" id="btnBack" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" /> <!-- Back button. -->
 					<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
-					<h2 id="headerId" style="font-weight: bold; style=display:inline-block; font-size:30px;">Personnel</h2> <!-- Heading containing name of page. -->
+					<h2 id="headerId" style="font-weight:bold; style=display:inline-block; font-size:30px;">Personnel</h2> <!-- Heading containing name of page. -->
 				</div>
 				<br/><br/>
 				<div class="row" align="center">
