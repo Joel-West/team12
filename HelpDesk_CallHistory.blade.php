75f8e9ca5148a8d329ec5cc2860d5ab5b2396a94
turnip99 <turnipz99@gmail.com>
2019-02-07 12:25:04 +0000

    Add files via upload

--- HelpDesk_CallHistory.blade.php
+++ HelpDesk_CallHistory.blade.php
@@ -8,83 +8,286 @@
 		<script type="text/javascript">
 			var userData; //Variable containing data about user.
 			var currentPage = "CallHistory"; //Variable storing the name of the current page, so it can be passed in the URL to the next page as a 'previous page' variable.
+			var selected = 0; //Global variable corresponding to number of highlighted table rows.
+			
 			function Load() //Function that runs when file loads.
 			{
 				userData = "<?php echo $_POST['User']; ?>"; //Gets data from previous form.
+				SetPrivileges(userData) //Enter function that defines what functions are available to user based on status.
+				ResetTable();
 				WriteTime(); //Function that writes the current time at the top of the page.
 			}
+			
+			function SetPrivileges(userData) //Function that checks if user is an admin or analyst and adjusts available buttons accordingly.
+			{
+				admin = (userData.split(","))[2];
+				analyst = (userData.split(","))[3]; //Retrieves admin and analyst status from userData that was earlier posted from previous form.
+				if (admin == 0 && analyst == 1)
+				{
+					$("#inputDiv :input").prop("disabled", true); //If not admin or operator, disable all input fields in the input div.
+					$("#inputDiv :textArea").prop("disabled", true);
+				}	
+			}
+			
+			function ResetTable()
+			{
+				if (document.getElementById("txtSearch").value == "") //If not searching anything.
+				{
+					sql = "SELECT tblCallHistory.*, tblProblem.problem, p1.name, p2.name FROM tblCallHistory INNER JOIN tblProblem ON tblCallHistory.problemNumber = tblProblem.problemNumber INNER JOIN tblPersonnel p1 ON tblCallHistory.operatorID = p1.userID INNER JOIN tblPersonnel p2 ON tblCallHistory.CallerID = p2.userID;"; //Simple query to get all data from table.
+					RunQuery(sql); //Runs function get gets data from database and display it in tableDiv.
+				}
+			}
+			
+			function Search() //Function for searching table based on text box input.
+			{
+				search = true; //Defines whether search will go ahead.
+				if (delList.length > 0 || updList.length > 0 || newRowCount > 0) //If there are pending changes.
+				{
+					if (!confirm("You have unsaved changes to the database. Searching will cause these changes to be cleared. Continue?")) //Warn user about losing data on searching.
+					{
+						return;
+					}
+					else
+					{
+						updList = []; //Clear lists of pending changes.
+						delList = [];
+						newRowCount = 0;
+					}
+				}
+				str = document.getElementById("txtSearch").value.toUpperCase(); //Gets uppercase array of searched text.
+				if (str.includes("'")) //If contains ' (if it is SQL injection-prone)
+				{
+					sql = "SELECT * FROM tblCallHistory WHERE 1 = 0;"; //Get no results.
+				}
+				else
+				{
+					
+					str = str.replace(", ", ",").split(","); //Split search text by commas.
+					sql = "SELECT tblCallHistory.*, tblProblem.problem, p1.name, p2.name FROM tblCallHistory INNER JOIN tblProblem ON tblCallHistory.problemNumber = tblProblem.problemNumber INNER JOIN tblPersonnel p1 ON tblCallHistory.operatorID = p1.userID INNER JOIN tblPersonnel p2 ON tblCallHistory.CallerID = p2.userID WHERE";
+					for (i = 0; i < str.length; i++) //Iterates through list of search terms, adding to the SQL query.
+					{
+						if (i != 0)
+						{
+							sql+=" OR ";
+						}
+						sql += "upper(callNumber) LIKE '%"+str[i]+"%' OR upper(operatorID) LIKE '%"+str[i]+"%' OR upper(callerID) LIKE '%"+str[i]+"%' OR upper(timeDate) LIKE '%"+str[i]+"%' OR upper(problemNumber) LIKE '%"+str[i]+"%' OR upper(notes) LIKE '%"+str[i]+"%' OR upper(problem) LIKE '%"+str[i]+"%' OR upper(p1.name) LIKE '%"+str[i]+"%' OR upper(p2.name) LIKE '%"+str[i]+"%'"; //Query that returns all database records with a cell containing search string.
+					}
+				}
+				RunQuery(sql); //Runs function get gets data from database and display it in tableDiv.
+			}
+			
+			function RunQuery(sql) //Function for running a query to the equipment table and getting building a table.
+			{
+				$.get("Query.php", {'sql':sql, 'returnData':true},function(json) //Calls query.php, which handles the SQL query and sorting of result data.
+				{
+					if(json && json[0]) //If result of php file was a json array.	
+					{				
+						var htm = "<table class='table' id='tbl' border='1'>";
+						htm+="<tr id='t0'><th onclick='SortTable(0)' scope='col'>Call Number</th>";
+						htm+="<th onclick='SortTable(1)' scope='col'>Operator</th>";
+						htm+="<th onclick='SortTable(2)' scope='col'>Caller</th>";
+						htm+="<th onclick='SortTable(3)' scope='col'>Time/Date</th>";
+						htm+="<th onclick='SortTable(4)' scope='col'>Problem</th>";
+						htm+="<th onclick='SortTable(5)'scope='col'>Notes</th></tr>"; //Appending column headers.
+						for (i = 0; i<json.length; i++) //Iterates through the json array of results.
+						{
+							htm += "<tr style='background-color:rgb(159, 255, 48);'>"; //Sets colour and ID of row.
+							htm +="<td>"+json[i].callNumber+" - "+json[i].p1.name"</td>";
+							htm +="<td>"+json[i].operatorID+" - "+json[i].p2.name"</td>";
+							htm +="<td>"+json[i].callerID+"</td>";
+							htm +="<td>"+json[i].timeDate+"</td>";
+							htm +="<td>"+json[i].problemNumber+" - "+json[i].problem"</td>";
+							htm +="<td>"+json[i].notes+"</td>";
+							htm += "</tr>";							
+						}
+					}
+					else
+					{
+						var htm = "Sorry, no results found..."; //If no results, display error.
+					}
+					document.getElementById("tableDiv").innerHTML = htm; //Appends HTML to tableDiv.
+					newRowCount = 0;
+				},'json');
+			}
+			
+			function CheckIfUpdate() //The 'add' button into an 'update' button and populate the text boxes, if exactly one row is selected.
+			{
+				if (selected == 1)
+				{
+					rowNum = GetSelectedRow(); //Gets the row that is selected.
+					document.getElementById("btnUpdate").disabled = false;
+					document.getElementById("txtNotes").disabled = false;
+					document.getElementById("txtNotes").value = document.getElementById("tbl").rows[rowNum].cells[6].innerHTML;
+				}
+				else
+				{
+					document.getElementById("btnUpdate").disabled = true;
+					document.getElementById("txtNotes").disabled = true;
+					document.getElementById("txtNotes").value = "";
+				}
+			}
+			
+			function ValidateInput() //Function returns true if the data input box is valid.
+			{
+				id = "txtNotes";
+				if (document.getElementById(id).value == false || document.getElementById(id).value.includes("'") || document.getElementById(id).value.length > 2047)
+				{
+					alert("Invalid notes."); //Returns error if data input from text box is invalid.
+					return false;
+				}
+				return true;
+			}
+			
+			function UpdateRow() //Function that updates the selected row.
+			{
+				if (!ValidateInput())
+				{
+					return;
+				}
+				row = document.getElementById("tbl").rows[GetSelectedRow()]; //Gets the details of the row that is selected.
+				row.cells[5].innerHTML = document.getElementById("txtNotes").value;
+				row.style.backgroundColor = '#9FFF30';
+				selected = 0;
+				CheckIfUpdateOrAdd();
+				if (!ListContains(updList, row.cells[0].innerHTML)) //If selected row is not already marked to be updated when changes are saved to the database later.
+				{
+					updList.push(row.cells[0].innerHTML); //Add the ID of the row to the list of rows to by updated when changes are commited to the actual database.
+					console.log(updList);
+				}
+			}
+			
+			function Delete() //Function for deleting selected rows from a table.
+			{
+				admin = (userData.split(","))[2];
+				if (selected == 0 || admin == 0) //if no rows are selected, or if not admin, leave function.
+				{
+					return;
+				}
+				if (confirm("Delete selected rows?")) //Get user confirmation.
+				{
+					rows = GetRows();
+					for (i = rows-1; i > 0; i--) //Iterate through the rows of the table.
+					{
+						deleteRow = false; //Variable holding if row will actually be deleted.
+						if (document.getElementById("tbl").rows[i].style.backgroundColor != 'rgb(159, 255, 48)') //If row is selected.
+						{
+							deleteRow = true;						
+						}
+						if (deleteRow == true) //If should be deleted after validation.
+						{
+							else
+							{
+								indexInUpdList = updList.indexOf(document.getElementById("tbl").rows[i].cells[0].innerHTML); //Get index of deleted item in update list.
+								if (indexInUpdList > -1)
+								{
+									updList.splice(indexInUpdList, 1); //Delete row from the update list - if record is deleted, it will not need to be updated.
+								}
+								delList.push(document.getElementById("tbl").rows[i].cells[0].innerHTML); //Add record id to list of rows that will be deleted from the actual database later.
+							}
+							document.getElementById("tbl").deleteRow(i); //Delete the row.
+						}
+					}
+					selected = 0;
+					console.log(delList);
+					CheckIfUpdateOrAdd();
+				}
+			}
+			
+			function SaveChanges() //Function that saves table data back to database.
+			{
+				admin = (userData.split(","))[2];
+				analyst = (userData.split(","))[3];
+				if (admin == 0 && analyst == 1) //If is not an operator or an admin, action is forbidden.
+				{
+					return;
+				}
+				sql = "";
+				for (i = 0; i < delList.length; i++) //Iterate through delete list (deletion performed first as it reduces database size, making other operations quicker).
+				{
+					sql+="DELETE FROM tblCallHistory WHERE callNumber = '" + delList[i] + "'; ";
+				}
+				for (i = 0; i < updList.length; i++) //Iterate through delete list (deletion performed first as it reduces database size, making other operations quicker).
+				{
+					callNumber = updList[i];
+					rowNum = GetRowWithID(serialNumber); //Gets the row number in the local table that corresponds to the call number in the updList.
+					if (rowNum != -1) //If row exists.
+					{
+						row = document.getElementById("tbl").rows[rowNum]; //Get row of local table that is being saved to database.
+						sql+="UPDATE tblCallHistory SET ";
+						sql+="notes = '"+ row.cells[5].innerHTML + "', ";
+						sql+="WHERE callNumber = '" + callNumber + "'; ";
+					}
+				}
+				alert(sql);
+				if (sql != "") //If there is any SQL to run.
+				{
+					$.get("Query.php", {'sql':sql, 'returnData':false},function(json) //Calls query.php, which handles the SQL query and sorting of result data.
+					{
+						if(json && json[0]) //If result of php file was a json array.	
+						{				
+							alert(json);
+							alert(json[0]);
+						}
+					},'json');
+					updList = []; //Clear lists of pending changes.
+					delList = [];
+					newRowCount = 0;
+					alert("Changes saved.");
+				}
+				else
+				{
+					alert("There are no changes to save.");
+				}
+			}
 		</script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> <!-- Bootstrap CSS stylesheet. -->
 		<link rel="stylesheet" href="{{ asset('css/Styles.css') }}" type="text/css"> <!-- Import external CSS stylesheet that contains presentation info that applies to all the pages. -->
+		<style>
+		.table-wrapper-scroll-y
+		{
+			display: block;
+			max-height: 850px;
+			overflow-y: auto;
+			-ms-overflow-style: -ms-autohiding-scrollbar;
+		}
+		</style>
 	</head>
-	<body onload="Load()">
-	<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
-		<input type='text' hidden id="user" name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
-		@csrf <!--Token to validates requests to server. -->
-		<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
-			<input type="button" id="btnBack" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" /> <!-- Back button. -->
-			<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
-			<h2 id="headerId" style="style=display:inline-block; font-size:30px;">Call History</h2>	 <!-- Heading containing name of page. -->
+	<body onload="Load()" style="height:100%;">
+		<div autocomplete="off" class="container-fluid"> <!-- Container holds elements together using Bootstrap. -->
+			<form id="mainform" name="mainform" method="post" action=""> <!-- This form will post data to an initially unspecified page when submitted. -->
+				<input type='text' hidden id="user" name="User"/> <!-- Hidden tag used to store posted user data so that it can later be posted back to the home page. -->
+				@csrf <!--Token to validates requests to server. -->
+				<div class="titleDiv"> <!-- Div containing elements at the top of the page. -->
+					<input type="button" id="btnBack" style="font-size:40px; position:absolute; left:0;" value="&#x2190" style="display:inline-block;" onClick="GoToNewPage('Home');" /> <!-- Back button. -->
+					<label id="dtLabel" style="font-size:26px; position:absolute; right:0;"></label> <!-- Label to contain current data/time. -->
+					<h2 id="headerId" style="font-weight:bold; style=display:inline-block; font-size:30px;"Call History</h2> <!-- Heading containing name of page. -->
+				</div>
+				<br/><br/>
+				<div class="row" align="center">
+					<div id="tableDiv" class="col-8 table-wrapper-scroll-y"> <!-- Div containing data table. -->
+						Loading data...
+					</div>
+					<br/>
+					<div id="rightDiv" align="center" class="col-4">
+						<div id="searchDiv">
+							<p>
+								Search:<input id="txtSearch" type="text" oninput="ResetTable()"></input> <!-- Box for searching the table for specific strings. -->
+								<input type="button" class="btn" id="btnSearch" value="Submit" onclick="Search()"></input> <!-- Submits search on press. -->
+							</p>
+						</div>
+						<div id="inputDiv">
+							<input type="button" class="btn" id="btnDelete" value="Delete Selected Items" id="del" style="font-size:16px;" onclick="Delete()"/><br/><br/> <!-- Delete button that calls function when pressed. -->
+							Notes:<br/><textArea id="txtNotes" type="text"></textArea><br/> <!-- Input field for updating notes. -->
+							<br/><input type="button" class="btn" id="btnUpdate" value="Update Item" style="font-size:16px;" onclick="UpdateRow()"></input>	
+							<br/>
+							<br/>
+							<p align="center">
+							<input type="button" id="btnSave" class="btn" value="Save Changes" style="font-size:26px; padding: 6px 12px;" onClick="SaveChanges();" /> <!-- Button for submitting changes to table. -->
+							</p>
+						</div>
+					</div>
+				</div>
+			</form>
 		</div>
-		<div id="tableDiv"> <!-- Div containing data table. -->
-			<table id="tbl" border="1">
-				<tr>
-					<th>Operator Name</th>
-					<th>Caller Name</th>
-					<th>Time/Date</th>
-					<th>Problem</th>
-					<th>Reason/Notes</th>
-				</tr>
-				<tr>
-					<td>Alice</td>
-					<td>Jim</td>
-					<td>12/10/2018, 11:43:20</td>
-					<td>MS Paint won't close</td>
-					<td>Still a problem, left PC for 3 days and no change. Worried new computer will be required.</td>
-				</tr>
-				<tr>
-					<td>Sam</td>
-					<td>Sarah</td>
-					<td>11/10/2018, 15:59:21</td>
-					<td>Broken Capslock</td>
-					<td>Caplock is not functioning.</td>
-				</tr>
-				<tr>
-					<td>Sam</td>
-					<td>Ryan</td>
-					<td>11/10/2018, 10:31:03</td>
-					<td>Chrome not running HTML</td>
-					<td>Running Chrome on Mac, HTML code does not display, creating incorrectly blank webpages.</td>
-				</tr>
-				<tr>
-					<td>Sam</td>
-					<td>Jenny</td>
-					<td>11/10/2018, 09:12:12</td>
-					<td>Overheated Computer</td>
-					<td>Computer won't turn on.</td>
-				</tr>
-				<tr>
-					<td>Alice</td>
-					<td>Ryan</td>
-					<td>10/10/2018, 13:15:54</td>
-					<td>Speakers Broken</td>
-					<td>Speakers crackle but no actual sound output.</td>
-				</tr>
-				<tr>
-					<td>Alice</td>
-					<td>Jenny</td>
-					<td>10/10/2018, 12:46:49</td>
-					<td>Overheated Computer</td>
-					<td>Computer crashes when overheating.</td>
-				</tr>
-				<tr>
-					<td>Alice</td>
-					<td>Jim</td>
-					<td>9/10/2018, 15:20:07</td>
-					<td>MS Paint won't close</td>
-					<td>MS Paint preventing computer from turning off.</td>
-				</tr>
-			</table>
-		</div>
-	</form>
 	</body>
 </html>
