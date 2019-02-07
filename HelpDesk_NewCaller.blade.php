39b0a0ed7ef635e17829ed59310332523477952b
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-07 15:13:10 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -291,6 +291,7 @@
 	  }
 	  
 	  function fillSpecialistComboBox(){
+		console.log("ENTERED COMBO");
 		var sql = "SELECT generalisation FROM tblProblemType WHERE typeName = '" + problemTypeVar + "';";
 		var html;
 		$.get("Query.php", {'sql':sql, 'returnData':true},function(json){
