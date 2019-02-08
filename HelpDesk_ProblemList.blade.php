3ab6caff68d19c23599f0ad3d5dd28ab36af3fe5
turnip99 <turnipz99@gmail.com>
2019-02-08 23:51:34 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -487,7 +487,7 @@
 				document.getElementById("typeSpecificDiv").text = htm; //Appends innerHTML for the input elements that change depending on the tab.
 				if (buttonPressed) //If entered via a button press, rather than my changing the tab of a record, set 'selected' to 0. Otherwise, it will remain at 1.
 				{
-					document.getElementById("tbl").rows[GetSelectedRow()].style='background-color:rgb(159, 255, 48);
+					document.getElementById("tbl").rows[GetSelectedRow()].style='background-color:rgb(159, 255, 48)';
 					selected = 0;
 					CheckIfUpdate() //Prevents user input if more or less than one row is selected.
 				}
