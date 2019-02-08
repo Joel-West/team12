85dbd63ea5816bd66154d4f2a30183dbabd18bee
turnip99 <turnipz99@gmail.com>
2019-02-08 23:00:15 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -439,7 +439,6 @@
 							sql = "SELECT * FROM tblProblem WHERE problemType = 'Hardware';"; //Simple query to get all hardware problem from table.
 							RunQuery(sql); //Runs function get gets data from database and display it in tableDiv.
 						}
-						tableDiv.innerHTML = hardwareHTML;
 						break;
 					case 'Software':
 						extraCells = 2; //There are two extra cells appended to the table when on the software tab (operating system, software concerned).
@@ -449,7 +448,6 @@
 							sql = "SELECT * FROM tblProblem WHERE problemType = 'Software';"; //Simple query to get all software problem from table.
 							RunQuery(sql); //Runs function get gets data from database and display it in tableDiv.
 						}
-						tableDiv.innerHTML = softwareHTML;
 						break;
 					case 'Network':
 						extraCells = 0; //There are no extra cells appended to the table when on the network tab.
@@ -459,7 +457,6 @@
 							sql = "SELECT * FROM tblProblem WHERE problemType = 'Network';"; //Simple query to get all network problem from table.
 							RunQuery(sql); //Runs function get gets data from database and display it in tableDiv.
 						}
-						tableDiv.innerHTML = networkHTML;
 						console.log("1" + tableDiv.innerHTML);
 						break;
 					default: break;
