731d0a22b97856d1c1d65b34a9895b03b07f7a1b
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-08 20:22:50 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -449,7 +449,6 @@
 		
 		<div class="col-3"></div>
 		<div class="collapse col-6" id="problemTypeCollapse">
-		
 		  Problem Type:
 		  <div id="problemTypeComboBox">
 		    <button class='btn greenBack dropdown-toggle' type='button' id='dropdownButton3' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
@@ -465,11 +464,13 @@
 		<div class="col-3"></div>
 		<div class="collapse col-6" id="serialNumberCollapse">
 		    Serial Number:
-		  <button class='btn greenBack dropdown-toggle' type='button' id='dropdownButtonSerial' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
-			Choose Serial Number<span class='caret'></span>
-	      </button>
-		  <div class='dropdown-menu' id='dropdown-menuSerial' aria-labelledby='dropdownMenuSerial'>
+		  <div id="serialNumberComboBox">
+		    <button class='btn greenBack dropdown-toggle' type='button' id='dropdownButtonSerial' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
+			  Choose Serial Number<span class='caret'></span>
+	        </button>
+		    <div class='dropdown-menu' id='dropdown-menuSerial' aria-labelledby='dropdownMenuSerial'>
 			  
+		    </div>
 		  </div>
 		</div>
 		<div class="col-3"></div>
