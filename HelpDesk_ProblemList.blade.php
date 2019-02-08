0bd0992497cd662083a57004899800e1f90a49a5
turnip99 <turnipz99@gmail.com>
2019-02-08 20:48:13 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -575,8 +575,9 @@
 						<label id="lblSpecialistNum"></label>
 						<br/>
 						Resolved? <input id="chkResolved" type="checkbox" onclick="CheckClicked()"></input><br/>
+												<br/>
 						<div id="solutionDiv">
-							<input id="txtDateTime" type="text" disabled></input>
+							<input id="txtDateTime" type="text" disabled></input><br/>
 							Solution:<br/><textArea class="form-control" rows="10" id="txtSolution" maxlength="2048" style="background-color:rgb(159, 255, 48);"></textArea><br/>
 						</div>
 						<br/><input type="button" class="btn" id="btnUpdate" value="Update Item" style="font-size:16px;" onclick="UpdateRow()"></input>	
