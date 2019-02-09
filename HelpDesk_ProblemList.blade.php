a632dbd9395ebae19db340e5ab719c9d8840f6a3
turnip99 <turnipz99@gmail.com>
2019-02-09 16:36:00 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -689,21 +689,23 @@
 				</div>
 				<br/><br/>
 				<div class="row" align="center">
-				<div id="leftDiv" align="center" class="col-9 table-wrapper-scroll-y">
+				<div id="leftDiv" align="center">
 						<div id="tabDiv" class="row" align="center" style="text-align:center; display: inline-block;"> <!-- Within this row are three buttons that change the tab of problems listed. -->
 							<input type="button" id="btnHardware" class="btn tabButton" value="Hardware" onclick="ChangeTab('Hardware', true)"></input>
 							<input type="button" id="btnSoftware" class="btn tabButton" value="Software" onclick="ChangeTab('Software', true)"></input>
 							<input type="button" id="btnNetwork" class="btn tabButton" value="Network" onclick="ChangeTab('Network', true)"></input>
 						</div>
 						<br/>
-						<div id="tableDivHardware" style="display:none"> <!-- Div containing hardware data table. -->
-							Loading data...
-						</div>
-						<div id="tableDivSoftware" style="display:none"> <!-- Div containing software data table. -->
-							Loading data...
-						</div>
-						<div id="tableDivNetwork" style="display:none"> <!-- Div containing network data table. -->
-							Loading data...
+						<div id="tableOuterDiv" class="col-9 table-wrapper-scroll-y">
+							<div id="tableDivHardware" style="display:none"> <!-- Div containing hardware data table. -->
+								Loading data...
+							</div>
+							<div id="tableDivSoftware" style="display:none"> <!-- Div containing software data table. -->
+								Loading data...
+							</div>
+							<div id="tableDivNetwork" style="display:none"> <!-- Div containing network data table. -->
+								Loading data...
+							</div>
 						</div>
 					<br/>
 				</div>
