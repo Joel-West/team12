ff72b5f7c956b7b7202be432fde3ffcfae9b6ea0
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-09 17:32:55 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -460,7 +460,7 @@
 		
 		<div class="col-3"></div>
 		<div class="collapse col-3" id="problemTypeCollapse">
-		  <label style="padding-left: 1vw">
+		  <label style="padding-left: 2vw">
 		    Problem Type:
 		  </label>
 		  <div id="problemTypeComboBox" class="text-right">
