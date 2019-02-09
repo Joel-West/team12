68171583dc694e73c4da1e283673e59fabbe5ee7
turnip99 <turnipz99@gmail.com>
2019-02-09 00:08:54 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -489,6 +489,10 @@
 				if (buttonPressed) //If entered via a button press, rather than my changing the tab of a record, set 'selected' to 0. Otherwise, it will remain at 1.
 				{
 					selected = 0;
+					if (document.getElementById("tbl") != null)
+					{
+						document.getElementById("tbl").rows[GetSelectedRow()].style='background-color:rgb(0, 255, 255)';
+					}
 					CheckIfUpdate() //Prevents user input if more or less than one row is selected.
 				}
 			}
