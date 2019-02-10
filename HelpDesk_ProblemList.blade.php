5ed2cf38e8a8cd30c7c6c7b998d4187c2313b8a6
turnip99 <turnipz99@gmail.com>
2019-02-10 22:08:59 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -274,9 +274,10 @@
 			
 			function FindAllProblemTypeChildren(parent) //Give it a generalisation and it will find all problem types which stem from this generalisation.
 			{
-				console.log(parent);
+				
 				for (i = 0; i < allProblemTypes.length; i++) //Iterates through array of all problem types to find types with the given generalisation.
 				{
+					console.log(allProblemTypes[i].generalisation);
 					if (allProblemTypes[i].generalisation == parent)
 					{
 						problemTypes.push(allProblemTypes[i].typeName);
