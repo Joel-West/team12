91699745020eca4cd1f3c7658376ab323a11127e
turnip99 <turnipz99@gmail.com>
2019-02-10 22:26:31 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -264,6 +264,7 @@
 			
 			function GetProblemTypeArray() //Function to get array of all the valid problem types for the current tab.
 			{
+				problemTypes.clear();
 				switch (extraCells)
 				{
 					case 0: FindAllProblemTypeChildren("Network problem"); break;
