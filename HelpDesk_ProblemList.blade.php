0ddaf348a61988ccd1900c58a75d0398ab2f2732
turnip99 <turnipz99@gmail.com>
2019-02-12 11:22:45 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -473,7 +473,7 @@
 					return; //If it is on the correct tab already, leave function.
 				}
 				row = document.getElementById(GetCurrentTableID()).rows[GetSelectedRow()]; //Gets the details of the row that is selected.
-				console.log(row.cells[2);
+				console.log(row.cells[2]);
 				row.cells[2] = "";
 				for (i = 0; i<extraCells; i++) //Clears tab-specific fields.
 				{
