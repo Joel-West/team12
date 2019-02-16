bf5005381640bfdeaefa97979975e2fbbb896c34
turnip99 <turnipz99@gmail.com>
2019-02-16 23:24:40 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -718,6 +718,7 @@
 				if (!document.getElementById("chkAllProblems").checked)
 				{
 					table.tBodies[0].innerHTML += "<tr class='rowDeselected' style='display: none;'>"+rowData+"</tr>"; //If being transferred such that it will become a hidden row, due to a specialist having selected only to view their own assigned problems, hide row after adding it to new tab's table.
+					selected = 0;
 				}
 				else
 				{
