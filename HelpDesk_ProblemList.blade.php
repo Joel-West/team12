845858cc3c473884b6e461c7ac8bb49a19da9c17
turnip99 <turnipz99@gmail.com>
2019-02-16 22:54:52 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -314,7 +314,7 @@
 					if (rows[0].style.display == "none") //If header was hidden from there being no relevant rows earlier.
 					{
 						rows[0].style.display = "";
-						div.innerHTML.replace("There are no problems to show for this type...", "");
+						body.innerHTML.replace("There are no problems to show for this type...", "");
 					}
 					for (j = 1; j < rows.length; j++) //Iterates through each row in the table.
 					{
