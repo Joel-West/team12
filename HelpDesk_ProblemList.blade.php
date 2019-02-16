9a3de60091deda26eb8b3f346e8892091ed589a1
turnip99 <turnipz99@gmail.com>
2019-02-16 23:43:50 +0000

    Add files via upload

--- HelpDesk_ProblemList.blade.php
+++ HelpDesk_ProblemList.blade.php
@@ -356,7 +356,6 @@
 			
 			CheckIfHideHeader(rows) //Checks if all rows of the table are hidden, and thus if the header should be hidden.
 			{
-				alert("hello there");
 				for (i = 1; i < rows.length; i++)
 				{
 					if (rows[i].style.display != "none")
