54340bcd5dee40578d53dfefdc460cb3672b3877
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-16 23:11:42 +0000

    Add files via upload

--- HelpDesk_NewCaller.blade.php
+++ HelpDesk_NewCaller.blade.php
@@ -155,16 +155,16 @@
 			$("#dropdownButtonSerial:first-child").text(json[0].serialNumber);
             $("#dropdownButtonSerial:first-child").val(json[0].serialNumber);
 		  }
-		},'json');
-		sql2 = "SELECT specialistID FROM tblProblem WHERE problemNumber = '" + problemNumber + "';";
-		console.log("AH"+problemNumber);
-		$.get("Query.php", {'sql':sql2, 'returnData':true},function(json){
-		  if(json && json[0]){
-			index = specialistIDList.indexOf(json[0].specialistID);
-			$("#dropdownButton4:first-child").text(specialistList[index] + " (" + count[index] + " current jobs) (" + json[0].specialistID + ")");
-			$("#dropdownButton4:first-child").val(specialistList[index] + " (" + count[index] + " current jobs) (" + json[0].specialistID + ")");
-		  }
+		  sql2 = "SELECT specialistID FROM tblProblem WHERE problemNumber = '" + problemNumber + "';";
+		  console.log("AH"+problemNumber);
+		  $.get("Query.php", {'sql':sql2, 'returnData':true},function(json){
+		    if(json && json[0]){
+			  index = specialistIDList.indexOf(json[0].specialistID);
+			  $("#dropdownButton4:first-child").text(specialistList[index] + " (" + count[index] + " current jobs) (" + json[0].specialistID + ")");
+			  $("#dropdownButton4:first-child").val(specialistList[index] + " (" + count[index] + " current jobs) (" + json[0].specialistID + ")");
+		    }
 		  sqlUpdate();
+		  },'json');
 		},'json');
 	  }
 	  
