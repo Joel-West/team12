e123382772cb9808d13538893d63d4b065f8a104
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-17 19:33:05 +0000

    Add files via upload

--- Hash.blade.php
+++ Hash.blade.php
@@ -1,6 +1,6 @@
 <?php
   use Illuminate\Support\Facades\Hash;
   $password = $_REQUEST['password'];
-  $hashed = Hash::make($password);
+  $hashed = Hash::make('password');
   echo (json_encode($hashed));
 ?>
