704f8aa1d79bff3830f447ab67473a58ba0106b0
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-17 19:00:45 +0000

    Update HelpDesk_Log.blade.php

--- HelpDesk_Log.blade.php
+++ HelpDesk_Log.blade.php
@@ -20,6 +20,9 @@
 			{
 				var Username=document.getElementById("Username").value; //Get username and password from text boxes.
 				var Password=document.getElementById("Password").value;
+				$.get("Hash.php", {'password':Password},function(hashed){
+				  Password == hashed;
+				},'hashed');
 				if (Username.includes("'")) //Protects against SQL injection.
 				{
 					return;
