8801474b02b6a78c2695a9811242835bbd6a51bc
Joel-West <45597363+Joel-West@users.noreply.github.com>
2019-02-17 20:57:40 +0000

    Add files via upload

--- Hash.php
+++ Hash.php
@@ -1,5 +1,6 @@
 <?php
   $Password = $_REQUEST['Password'];
-  $Hashed = (object) $Password;
+  $Hashed = array('hash' => password_hash($Password, PASSWORD_DEFAULT));
+  $Hashed = (object) $Hashed;
   echo json_encode($Hashed);
 ?>
